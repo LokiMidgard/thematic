7978afd3f2f24f00fd00787e1f86b338f5211c55
Joseph <Joseph@localhost>
2010-12-06 22:29:28 +1000

    improved project generation...

---
+++ Source/ThemeMatic.Test/Model/SimpleProjectGeneratorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ThemeMatic.Model;
+using Xunit;
+
+namespace ThemeMatic.Test.Model
+{
+    public class SimpleProjectGeneratorTest
+    {
+        [Fact]
+        public void NewProjectTextCanBeGeneratedFromDesign()
+        {
+            var simpleProjectGenerator = new SimpleProjectGenerator(Path.GetTempPath());
+            var newProjectContent = simpleProjectGenerator.GenerateNewProjectFileContents(null, new List<string>(){"..\\foo\\bar\\test.xaml"});
+            Assert.True(!string.IsNullOrEmpty(newProjectContent));
+        }
+    }
+}
--- Source/ThemeMatic/Model/SimpleProjectGenerator.cs
+++ Source/ThemeMatic/Model/SimpleProjectGenerator.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Windows.Markup;
+using NDepend.Helpers.FileDirectoryPath;
 
 namespace ThemeMatic.Model
 {
@@ -36,30 +37,69 @@ namespace ThemeMatic.Model
                 throw new ArgumentNullException("design");
             }
             string folder = CreateFolderStructure(design);
-            WriteResourceDictionaries(design, folder);
-            WriteProject(design);
+            
+            var resourceDictionaries = new List<string>();
+            WriteResourceDictionaries(design, folder, resourceDictionaries);
+            WriteProject(design, resourceDictionaries);
         }
 
-        private void WriteProject(Design design)
+        private void WriteProject(Design design, List<string> resourceDictionaries)
         {
-            // TODO
+            string projectFileName = Path.Combine(ThemeProjectDirectory(design), design.ThemeAssemblyName + ".csproj");
+            if (!File.Exists(projectFileName))
+            {
+                List<string> relativePaths = GetRelativePathsForResourceDictionaries(design, resourceDictionaries);
+
+                using (var fileStream = new StreamWriter(projectFileName, true))
+                {
+                    fileStream.Write(GenerateNewProjectFileContents(design, relativePaths));
+                    fileStream.Flush();
+                }
+            }
         }
 
-        private void WriteResourceDictionaries(Design design, string wpfFolderName)
+        private List<string> GetRelativePathsForResourceDictionaries(Design design, List<string> resourceDictionaries)
         {
-            File.Copy(Path.Combine(resourceRootPath, themePathFragment, design.Theme.Name + ".xaml"), Path.Combine(wpfFolderName, design.Theme.Name + ".xaml"));
-            CopyAll(new DirectoryInfo(Path.Combine(themePathFragment, design.Theme.Name)), new DirectoryInfo(Path.Combine(wpfFolderName, design.Theme.Name)));
+            var projectRootDirectory = ThemeProjectDirectory(design);
+            var absoluteProjectRoot = new DirectoryPathAbsolute(projectRootDirectory);
+
+            var relativePaths = new List<string>();
+            foreach (var path in resourceDictionaries)
+            {
+                var absoluteFile = new FilePathAbsolute(path);
+                var relativeFile = absoluteFile.GetPathRelativeFrom(absoluteProjectRoot);
+                relativePaths.Add(relativeFile.Path);
+            }
+            return relativePaths;
+        }
+
+        private void WriteResourceDictionaries(Design design, string wpfFolderName, List<string> resourceDictionariesCopied)
+        {
+
+            string targetBaseResourceDictionaryName = Path.Combine(wpfFolderName, design.Theme.Name + ".xaml");
+            File.Copy(Path.Combine(resourceRootPath, themePathFragment, design.Theme.Name + ".xaml"), targetBaseResourceDictionaryName);
+            resourceDictionariesCopied.Add(targetBaseResourceDictionaryName);
+
+            CopyAll(new DirectoryInfo(Path.Combine(themePathFragment, design.Theme.Name)), new DirectoryInfo(Path.Combine(wpfFolderName, design.Theme.Name)), resourceDictionariesCopied);
+        }
+
+        public string BaseDirectory(Design design)
+        {
+            return Path.Combine(design.OutputLocation, design.ProjectName);
+        }
+
+        public string ThemeProjectDirectory(Design design)
+        {
+            return Path.Combine(BaseDirectory(design), design.ThemeAssemblyName);
         }
 
         private string CreateFolderStructure(Design design)
         {
-            string baseDirectory = Path.Combine(design.OutputLocation, design.ProjectName);
-            string themeProjectDirectory = Path.Combine(baseDirectory, design.ThemeAssemblyName);
-            CreateIfNotExists(baseDirectory);
-            CreateIfNotExists(themeProjectDirectory);
+            CreateIfNotExists(BaseDirectory(design));
+            CreateIfNotExists(ThemeProjectDirectory(design));
 
             // create \Resources\Themes\Wpf\<theme name>"
-            string resources = Path.Combine(themeProjectDirectory, "Resources");
+            string resources = Path.Combine(ThemeProjectDirectory(design), "Resources");
             CreateIfNotExists(resources);
 
             string themes = Path.Combine(resources, "Themes");
@@ -79,8 +119,7 @@ namespace ThemeMatic.Model
             }
         }
 
-        // copied straight form the MSDN...the shame
-        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target, List<string> filesCopied)
         {
             if (source.FullName.ToLower() == target.FullName.ToLower())
             {
@@ -94,9 +133,11 @@ namespace ThemeMatic.Model
             }
 
             // Copy each file into it's new directory.
-            foreach (FileInfo fi in source.GetFiles())
+            foreach (FileInfo fi in source.GetFiles().Where(a => a.Extension.ToLower() == ".xaml"))
             {
-                fi.CopyTo(Path.Combine(target.ToString(), fi.Name), true);
+                string targetFileName = Path.Combine(target.ToString(), fi.Name);
+                fi.CopyTo(targetFileName, true);
+                filesCopied.Add(targetFileName);
             }
 
             // Copy each subdirectory using recursion.
@@ -104,8 +145,84 @@ namespace ThemeMatic.Model
             {
                 DirectoryInfo nextTargetSubDir =
                     target.CreateSubdirectory(diSourceSubDir.Name);
-                CopyAll(diSourceSubDir, nextTargetSubDir);
+                CopyAll(diSourceSubDir, nextTargetSubDir, filesCopied);
             }
         }
+
+        public string GenerateNewProjectFileContents(Design design, List<string> relativePathsFromProjectRootToXamlFiles)
+        {
+            string xamlFileNodes = GenerateXamlFileNodeFragment(relativePathsFromProjectRootToXamlFiles);
+            return string.Format(projectSkeleton, Guid.NewGuid(), xamlFileNodes);
+        }
+
+        private string GenerateXamlFileNodeFragment(List<string> relativePathsFromProjectRootToXamlFiles)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in relativePathsFromProjectRootToXamlFiles)
+            {
+                sb.Append(
+                    string.Format(
+                        @"
+    <Page Include='{0}'>
+      <SubType>Designer</SubType>
+      <Generator>MSBuild:Compile</Generator>
+    </Page>",
+                        item));
+            }
+            return sb.ToString();
+        }
+
+        private const string projectSkeleton = @"<?xml version='1.0' encoding='utf-8'?>
+<Project ToolsVersion='4.0' DefaultTargets='Build' xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
+  <PropertyGroup>
+    <Configuration Condition="" '$(Configuration)' == '' "">Debug</Configuration>
+    <Platform Condition="" '$(Platform)' == '' "">AnyCPU</Platform>
+    <ProductVersion>8.0.30703</ProductVersion>
+    <SchemaVersion>2.0</SchemaVersion>
+    <ProjectGuid>{{{0}}}</ProjectGuid>
+    <OutputType>Library</OutputType>
+    <AppDesignerFolder>Properties</AppDesignerFolder>
+    <RootNamespace>ThemeTest</RootNamespace>
+    <AssemblyName>ThemeTest</AssemblyName>
+    <TargetFrameworkVersion>v4.0</TargetFrameworkVersion>
+    <FileAlignment>512</FileAlignment>
+  </PropertyGroup>
+  <PropertyGroup Condition="" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' "">
+    <DebugSymbols>true</DebugSymbols>
+    <DebugType>full</DebugType>
+    <Optimize>false</Optimize>
+    <OutputPath>bin\Debug\</OutputPath>
+    <DefineConstants>DEBUG;TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <PropertyGroup Condition="" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' "">
+    <DebugType>pdbonly</DebugType>
+    <Optimize>true</Optimize>
+    <OutputPath>bin\Release\</OutputPath>
+    <DefineConstants>TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+<ItemGroup>
+    <Reference Include='PresentationCore' />
+    <Reference Include='PresentationFramework' />
+    <Reference Include='ReachFramework' />
+    <Reference Include='System' />
+    <Reference Include='System.Core' />
+    <Reference Include='Microsoft.CSharp' />
+    <Reference Include='System.Xaml' />
+    <Reference Include='WindowsBase' />
+    <Reference Include='PresentationUI, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35, processorArchitecture=MSIL'>
+      <SpecificVersion>False</SpecificVersion>
+      <HintPath>C:\Windows\Microsoft.NET\Framework\v3.0\WPF\PresentationUI.dll</HintPath>
+    </Reference>
+  </ItemGroup>
+  <ItemGroup>
+    {1}
+  </ItemGroup>
+  <Import Project='$(MSBuildToolsPath)\Microsoft.CSharp.targets' />
+</Project>
+";
     }
 }

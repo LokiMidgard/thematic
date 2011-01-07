401478648495810b6b196bc14bb03592519ade5a
Joseph <Joseph@localhost>
2011-01-08 01:45:51 +1000

    minor tweaks to both windows, fixed a bug I introduced when I changed the name of the generated executable from ThemeMatic to TheMatic.

--- Source/ThemeMatic/Model/ThemeFactory.cs
+++ Source/ThemeMatic/Model/ThemeFactory.cs
@@ -11,9 +11,9 @@ namespace ThemeMatic.Model
         public List<Theme> GetAllThemes()
         {
             var themes = new List<Theme>();
-            themes.Add(new Theme() { Description = "A simple, clean control theme originally developed by Robbie Ingebretsen", Name = "SimpleStyles", BaseResourceDictionaryUrl = "/Thematic;component/Resources/Themes/WPF/SimpleStyles.xaml", PreviewImage = new BitmapImage(new Uri("/Themematic;component/Resources/Themes/WPF/SimpleStyles/preview.png", UriKind.Relative))});
-            themes.Add(new Theme() { Description = "A modern theme inspired by the Zune and Windows Phone 7 UI. Originally released by Microsoft for Silverlight.", Name = "Cosmopolitan", BaseResourceDictionaryUrl = "/Thematic;component/Resources/Themes/WPF/Cosmopolitan.xaml", PreviewImage = new BitmapImage(new Uri("/Themematic;component/Resources/Themes/WPF/Cosmopolitan/preview.png", UriKind.Relative)) });
-            themes.Add(new Theme() { Description = "A modern theme for business applications. Originally released by Microsoft for Silverlight.", Name = "JetPack", BaseResourceDictionaryUrl = "/TheMatic;component/Resources/Themes/WPF/JetPack.xaml", PreviewImage = new BitmapImage((new Uri("/Themematic;component/Resources/Themes/WPF/JetPack/preview.png", UriKind.Relative)))});
+            themes.Add(new Theme() { Description = "A simple, clean control theme originally developed by Robbie Ingebretsen", Name = "SimpleStyles", BaseResourceDictionaryUrl = "/Thematic;component/Resources/Themes/WPF/SimpleStyles.xaml", PreviewImage = new BitmapImage(new Uri("/Thematic;component/Resources/Themes/WPF/SimpleStyles/preview.png", UriKind.Relative))});
+            themes.Add(new Theme() { Description = "A modern theme inspired by the Zune and Windows Phone 7 UI. Originally released by Microsoft for Silverlight.", Name = "Cosmopolitan", BaseResourceDictionaryUrl = "/Thematic;component/Resources/Themes/WPF/Cosmopolitan.xaml", PreviewImage = new BitmapImage(new Uri("/Thematic;component/Resources/Themes/WPF/Cosmopolitan/preview.png", UriKind.Relative)) });
+            themes.Add(new Theme() { Description = "A modern theme for business applications. Originally released by Microsoft for Silverlight.", Name = "JetPack", BaseResourceDictionaryUrl = "/TheMatic;component/Resources/Themes/WPF/JetPack.xaml", PreviewImage = new BitmapImage((new Uri("/Thematic;component/Resources/Themes/WPF/JetPack/preview.png", UriKind.Relative)))});
             return themes;
         }
     }
